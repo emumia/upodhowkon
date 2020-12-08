589ae62baf7f0ad10f31d50ed8bcec174172a507
MD IMARN <emukhan568@gmail.com>
2020-12-08 14:35:23 +0600

    Update models.py
    rom PIL import Image
    #add the method under Product class
    def save(self,*args, **kwargs):
    		super().save(*args, **kwargs)
    		img = Image.open(self.image.path)
    
    		if img.height > 250 or img.weight > 450:
    			output_size = (350,600)
    			img.thumbnail(output_size)
    			img.save(self.image.path)

--- store/models.py
+++ store/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
+
 
 # Create your models here.
 
@@ -17,6 +19,15 @@ class Product(models.Model):
 	price = models.FloatField()
 	digital = models.BooleanField(default=False,null=True, blank=True)
 	image = models.ImageField(null=True, blank=True)
+	
+	def save(self,*args, **kwargs):
+		super().save(*args, **kwargs)
+		img = Image.open(self.image.path)
+
+		if img.height > 250 or img.weight > 450:
+			output_size = (350,600)
+			img.thumbnail(output_size)
+			img.save(self.image.path)
 
 	def __str__(self):
 		return self.name
